396ba70cb0862215d53da127272eaae4b5b65efa
Gabor Szabo <gabor@szabgab.com>
2026-01-03 10:45:04 +0200

    move test to be an integration test

--- src/macros.rs
+++ src/macros.rs
@@ -481,31 +481,4 @@ mod tests {
             }
         }
     }
-
-    #[test]
-    fn test_default_fmt_val() {
-        make_config! {
-            struct MyConfig {
-              url: {
-                var_name: "MY_URL",
-                description: "Some URL",
-              }
-            }
-        }
-
-        unsafe {
-            std::env::set_var("MY_URL", "asd");
-        }
-        let config = MyConfig::define();
-        config.init();
-        assert_eq!(config.url.get(), "asd");
-        assert_eq!(config.url.get_descriptor().description, Some("Some URL"));
-        assert_eq!(config.url.get_descriptor().var_name, "MY_URL");
-        assert_eq!(config.url.get_descriptor().default_val_fmt, None);
-
-        let url = config.url.default_fmt_val("hi");
-        assert_eq!(url.get_descriptor().default_val_fmt, Some("hi"));
-        assert_eq!(url.get_descriptor().description, Some("Some URL"));
-        assert_eq!(url.get_descriptor().var_name, "MY_URL");
-    }
 }
